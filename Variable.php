8f4144323ca0c2022fbeeb33c19e9409aca70b95
wahyu <wahyu.creator911@gmail.com>
2017-04-13 14:14:29 +0700

    master

---
+++ Variable.php
@@ -0,0 +1,58 @@
+<?php
+
+$jenis_pembiayaan['jenis_pembiayaan'] = [
+    'Murabahah',
+    'Qardhul Hasan',
+    'Musyarakah',
+    'Ijaroh',
+    'Mudharabah'
+];
+
+$usaha['pekerjaan'] = [
+    'Usaha',
+    'Non Usaha'
+];
+
+$tanggungan['jumlah_tanggungan'] = [
+    '0-2 Orang',
+    '>=3 Orang'
+];
+
+$pembiayaan = [
+    '<5 Juta',
+    '5 - 50 Juta',
+    '>50 Juta',
+];
+
+$keperluan = [
+    'Tambah Modal',
+    'Beli Barang'
+];
+
+$jangka_waktu = [
+    '1 - 12 Bulan',
+    '13 - 24 Bulan',
+    '25 - 36 Bulan',
+];
+
+$agunanjaminan = [
+    '<10 Juta',
+    '10 - 50 Juta',
+    '>50 Juta',
+];
+
+$total_penghasilan = [
+    '< 4 Juta',
+    '4 - 7 Juta',
+    '> 7 Juta',
+];
+
+$karakter = [
+    'Baik',
+    'Bermasalah',
+];
+
+$kelayakan = [
+    'Ya',
+    'Tidak',
+];
---
+++ proses_algoritma_running.php
@@ -0,0 +1,18 @@
+<?php
+
+include './koneksi.php';
+
+class runningAlgoritm{
+    
+    function __construct() {
+        
+    }
+    
+    static function generate(){
+        
+    }
+    
+    static function getCountColomn(){
+        $result = mysql_query("select from data_nasabah");
+    }
+}
